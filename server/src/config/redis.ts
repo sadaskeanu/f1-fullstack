92498054825006adeadddb59ddfdfe95d8c937a9
Daria <dariak@hexaware.com>
2025-05-31 23:30:19 +0200

    chore(redis): support Redis connection in both local and Railway environments

--- server/src/config/redis.ts
+++ server/src/config/redis.ts
@@ -1,7 +1,10 @@
 import Redis from "ioredis";
 
-const redis = new Redis({
-  host: process.env.REDIS_HOST || "localhost",
-  port: 6379,
-});
+const redis = process.env.REDIS_URL
+  ? new Redis(process.env.REDIS_URL)
+  : new Redis({
+      host: process.env.REDIS_HOST || "localhost",
+      port: Number(process.env.REDIS_PORT) || 6379,
+    });
+
 export default redis;
