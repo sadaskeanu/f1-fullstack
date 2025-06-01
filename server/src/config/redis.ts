52a6ffc44461755bdf6928323a3f3909ee5641f6
Daria <dariak@hexaware.com>
2025-06-01 09:19:16 +0200

    feat(redis): console to debug REDIS_PUBLIC_URL

--- server/src/config/redis.ts
+++ server/src/config/redis.ts
@@ -4,8 +4,8 @@ let redis: Redis;
 
 export default function getRedis(): Redis {
   if (!redis) {
-    const url = process.env.REDIS_URL;
-    console.log("REDIS_URL:", process.env.REDIS_URL);
+    const url = process.env.REDIS_PUBLIC_URL;
+    console.log("REDIS_URL:", process.env.REDIS_PUBLIC_URL);
     if (url) {
       const redisUrl = new URL(url);
       redis = new Redis({
